d0e5bbd49c49d0b3f6a46c0d4ea4db1db8014761
mitrofanzxc <mitrofanzxc@gmail.com>
2022-08-19 17:07:50 +0300

    feat: add listeners for arrow buttons on keyboard

--- rslang/src/pages/Sprint/components/game/Game.tsx
+++ rslang/src/pages/Sprint/components/game/Game.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from 'react';
+import { useEffect, MouseEvent } from 'react';
 import { ButtonSelectList, ButtonReset } from '..';
 
 export interface IGame {
@@ -12,6 +12,29 @@ export function Game({ level, onClick }: IGame) {
     const textContent = target.textContent as string;
     console.log('textContent ===', textContent);
   };
+
+  const handleKeySelect = (event: globalThis.KeyboardEvent) => {
+    const { code } = event;
+
+    switch (code) {
+      case 'ArrowRight':
+        console.log('code ===', code);
+        break;
+      case 'ArrowLeft':
+        console.log('code ===', code);
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeySelect);
+
+    return () => {
+      document.removeEventListener('keypress', handleKeySelect);
+    };
+  }, []);
 
   return (
     <div className="sprint-frame">
