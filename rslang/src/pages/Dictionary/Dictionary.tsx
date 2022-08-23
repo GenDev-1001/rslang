fd374cb1b87a58f1f030c3567007a37bc90760a3
GenDev-1001 <evgenii.ubileii@bk.ru>
2022-08-23 14:23:13 +0300

    feat: added sort by group

--- rslang/src/pages/Dictionary/Dictionary.tsx
+++ rslang/src/pages/Dictionary/Dictionary.tsx
@@ -4,10 +4,7 @@ import { Footer } from '../../components/Footer/Footer';
 import { LevelCard } from '../../components/LevelCard/LevelCard';
 import Pagination from '../../components/Pagination/Pagination';
 
-import {
-  useActiveWordsByUserQuery,
-  useCountWordsByGroupQuery,
-} from '../../features/aggregaredWords/aggregaredWordsApiSlice';
+import { useActiveWordsByUserQuery } from '../../features/aggregaredWords/aggregaredWordsApiSlice';
 import { useAuth } from '../../hooks/useAuth';
 import './Dictionary.scss';
 
@@ -17,12 +14,17 @@ export function Dictionary() {
   } = useAuth();
 
   const [currentPage, setCurrentPage] = useState(1);
-  const { data: countWords } = useCountWordsByGroupQuery({ userId: userId || '', group: 0 });
+  const [currentGroup, setCurrentGroup] = useState(1);
+
   const { data: activeWords } = useActiveWordsByUserQuery({
     userId: userId || '',
-    group: 0,
-    page: 0,
+    group: currentGroup,
+    page: currentPage - 1,
   });
+
+  const handleClickGroup = (group: number) => {
+    setCurrentGroup(group);
+  };
 
   return (
     <>
@@ -34,12 +36,24 @@ export function Dictionary() {
         </div>
         <h4 className="dictionary-lvl__title">Уровни сложности слов</h4>
         <div className="dictionary-lvls__wrapper">
-          <LevelCard levelWord="Easy" range="1-600" levelIndex="A1" />
-          <LevelCard levelWord="Easy" range="601-1200" levelIndex="A1" />
-          <LevelCard levelWord="Medium" range="1201-1800" levelIndex="B1" />
-          <LevelCard levelWord="Medium" range="1801-2400" levelIndex="B2" />
-          <LevelCard levelWord="Hard" range="2401-3000" levelIndex="C1" />
-          <LevelCard levelWord="Hard" range="3001-3600" levelIndex="C2" />
+          <li onClick={() => handleClickGroup(0)}>
+            <LevelCard levelWord="Easy" range="1-600" levelIndex="A1" />
+          </li>
+          <li onClick={() => handleClickGroup(1)}>
+            <LevelCard levelWord="Easy" range="601-1200" levelIndex="A1" />
+          </li>
+          <li onClick={() => handleClickGroup(2)}>
+            <LevelCard levelWord="Medium" range="1201-1800" levelIndex="B1" />
+          </li>
+          <li onClick={() => handleClickGroup(3)}>
+            <LevelCard levelWord="Medium" range="1801-2400" levelIndex="B2" />
+          </li>
+          <li onClick={() => handleClickGroup(4)}>
+            <LevelCard levelWord="Hard" range="2401-3000" levelIndex="C1" />
+          </li>
+          <li onClick={() => handleClickGroup(5)}>
+            <LevelCard levelWord="Hard" range="3001-3600" levelIndex="C2" />
+          </li>
         </div>
         <div className="dictionary-lvls-custom__wrapper">
           <LevelCard levelWord="Сложные" range="Слов: 0" levelIndex="С" />
@@ -52,12 +66,14 @@ export function Dictionary() {
           })}
         </div>
         <Pagination
+          className="pagination"
           currentPage={currentPage}
           total={600}
           pageSize={20}
           onPageChange={(page) => setCurrentPage(page)}
         />
       </div>
+
       <Footer />
     </>
   );
